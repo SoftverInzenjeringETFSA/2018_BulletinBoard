a1777b4bb0e65652685fe10a33662745faf050f0
Kenan Hadžirović <khadzirovi1@etf.unsa.ba>
2018-04-30 23:08:37 +0200

    Opisi API-ja prije postavljanja arhitekture frontenda

--- SI Project/bulletin-board-front/src/App.js
+++ SI Project/bulletin-board-front/src/App.js
@@ -12,9 +12,14 @@ class App extends Component {
           <p>Stranica namjenjena boljem shvatanju arhitekture aplikacije za sve developere koji u budućnosti mogu raditi na razvoju i održavanju.</p>
         </header>
 
-        <p className="App-intro">
-          --
-        </p>
+        <div className="App-help">
+          <p>  
+            <b>UserController</b> - API kontroler povezan sa dashboard i postavke pogledima. Sadrži pozive metoda koji se tiču obrade podataka za korisnika i njegovih ličnih podataka.</p>
+          <p>  
+            <b>PostController</b> - API kontroler povezan sa dashboard pogledom. Sadrži pozive metoda koji se tiču obrade podataka za postove. Postove je moguće dodavati, uređivati i brisati. 
+            Sva logika je implementirana putem ovog kontrolera.
+          </p>
+        </div>
       </div>
     );
   }
