2b1679b07c8d8f2a619422c2a4f7fa8984da8e47
Irhad <irhadhalilovic@hotmail.com>
2018-04-30 23:49:30 +0200

    arhitektura backenda

---
+++ SI Project/bulletin-board/src/main/java/bulletinboard/project/bulletinboard/Controllers/PostController.java
@@ -0,0 +1,8 @@
+package bulletinboard.project.bulletinboard.Controllers;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+}
---
+++ SI Project/bulletin-board/src/main/java/bulletinboard/project/bulletinboard/Domain/Models/User.java
@@ -0,0 +1,10 @@
+package bulletinboard.project.bulletinboard.Domain.Models;
+
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private String FirstName;
+
+
+}
