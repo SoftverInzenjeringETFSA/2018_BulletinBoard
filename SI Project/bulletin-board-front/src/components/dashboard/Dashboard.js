d73b75ed86f047b73387dfca731209faa53bebd8
Haris Halilovic <hhalilovic1@etf.unsa.ba>
2018-05-17 01:26:21 +0200

    Added most of the hide post logic on frontend

--- SI Project/bulletin-board-front/src/components/dashboard/Dashboard.js
+++ SI Project/bulletin-board-front/src/components/dashboard/Dashboard.js
@@ -1,12 +1,63 @@
 import React, { Component } from 'react';
 import './Dashboard.css';
+import Post from '../post/Post';
 
 class Dashboard extends Component {
   render() {
+    
     return (
-      <div className="Dashboard">
-        DASHBOARD
+      <body background="pinboard.jpg">
+      <div id="glavniKontejner">
+        <div id="side">
+  
+            <div >
+            </div>
+            <div >
+                <i id="user-icon" class="fa fa-user"></i>
+            <i id="plus-icon"class="fa fa-plus"></i>
+                <div class="bottom-link">
+                    <a  id="logout">Log out</a>
+                </div>
+                    
+            </div>
+        </div >
+
+      <div id="kontejner">
+              <h1 class="site-title" id="naslov">Naslovnica</h1>
+              
+              <div id="stiker1"> 
+                  <p>     </p>
+                  <Post/>
+                  <div class="stiker_in">
+                      <p>Opis slike <i class="fa fa-ellipsis-h"></i></p>
+                  </div>
+              </div>
+
+              <div id="stiker2">
+                      <i class="fa fa-ellipsis-h"></i>
+                      <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                          Velit rerum odit quisquam. Ducimus dolores cupiditate cumque, 
+                          magnam quae ut reprehenderit perferendis commodi.</p>
+                  
+              </div>
+
+              <div id="stiker3"> 
+                  <p>      </p>
+                  <div class="stiker_in">
+                      <p>Opis slike <i class="fa fa-ellipsis-h"></i></p>
+                  </div>
+              </div>
+
+              <div id="stiker4">
+                  <p>      </p>
+                  <div class="stiker_in">
+                      <p>Opis slike <i class="fa fa-ellipsis-h"></i></p>
+                  </div>
+              </div>
+
       </div>
+</div>
+      </body>
     );
   }
 }
--- SI Project/bulletin-board-front/src/components/post/Post.js
+++ SI Project/bulletin-board-front/src/components/post/Post.js
@@ -1,9 +1,112 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Post.css';
+import axios from 'axios'
 
 class Post extends Component {
+	
+	
+	constructor(props) {
+        super(props);
+        this.state = {
+				UserId: Number,
+				Title: String,
+				Description: String,
+				Color: String,
+				DateCreated: Date,
+				IsHidden: true,
+				Date: Date
+		};
+		this.fetchPost = this.fetchPost.bind(this);
+		this.hidePost = this.hidePost.bind(this);
+	}
+
+	ping() {
+		// console.log("Ping was clicked");
+		// alert("Hello! I am an alert box!!");
+		
+		axios.get("http://localhost:8080/getAllPostsWithDate").then(res => {
+				alert("Received Successful response from server!"+res);
+			}, err => {
+		alert("Server rejected response with: " + err);
+  });
+	}
+
+	//Future service to get post...
+	fetchPost() {
+			// Post = {
+			// 	UserId: Number,
+			// 	Title: String,
+			// 	Description: String,
+			// 	Color: String,
+			// 	DateCreated: Date,
+			// 	IsHidden: true,
+			// 	Date: Date
+			// };
+			const PostTemp = {
+				UserId: 1,
+				 Title: "VeryCohlTitle",
+				 Description: "String",
+				 Color: "String",
+				 DateCreated: Date.now().toString(),
+				 IsHidden: "Boolean",
+				 Date: "Date"
+			};
+			return PostTemp;
+	}
+
+
+	
+	hidePost(){
+		var temp = this.fetchPost();//.then(response => {
+			if(this.state.IsHidden == true)
+			{
+				this.setState({
+					IsHidden : false
+				});
+			}
+			else
+			{
+				this.setState({
+					IsHidden : true
+				});
+			}
+			
+			// this.setState((prevState, props) => ({
+			// 	IsHidden : prevState.IsHidden
+			//   }));
+		//});
+	}
+
+	
+		// if(this.state.IsHidden == false)
+		// {
+		// 	this.setState({
+		// 		IsHidden: true
+		// 	  });
+		// }
+	
+	  
 	render() {
-		return <div className="section" />;
+		
+		
+	return (
+			<div className="section" >
+			<div>
+				{this.fetchPost().UserId} <br/>
+				{this.fetchPost().Title}  <br/>
+				{this.fetchPost().DateCreated}<br/>
+				{this.state.IsHidden.toString()}
+			</div>
+			<div></div>
+			<div></div>
+				<div>
+					<button onClick={this.ping}>Ping!</button>
+					<button onClick={this.hidePost}>Hide!</button>
+				</div>
+			</div>
+		);
 	}
 }
+
+export default Post;
