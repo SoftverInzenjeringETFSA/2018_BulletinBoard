ace338cef3708331dd99a004d0294a21de4a3518
lhadzihasa1 <lhadzihasa1@etf.unsa.ba>
2018-05-17 12:01:16 +0200

    Dodan dio za dodavanje slika

--- SI Project/bulletin-board-front/src/components/image/Image.js
+++ SI Project/bulletin-board-front/src/components/image/Image.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Image extends React.Component{
     constructor(props){
       super(props);
       this.state = {
-        rotation: 0
+        rotation: 0,
+        selectedFile:null
       }
       this.source = props.source;
       
       this.rotate = this.rotate.bind(this);
+    }
+
+    fileSelectedHandler=event=>{
+       this.setState({
+         selectedFile:event.target.files[0]
+    })
+  }
+
+    fileUploadHandler=()=>{
+      const fd=new FormData();
+      fd.append('image',this.state.selectedFile,this.state.selectedFile.name);
+       axios.post('',fd)
+      
     }
     
     rotate(){
@@ -24,6 +39,8 @@ class Image extends React.Component{
     render(){
       const { rotation } =  this.state;
       return <div className="App-image">
+               <input type="file" onChange={this.fileSelectedHandler}/>
+               <button onClick={this.fileUploadHandler}>Upload</button>
                 <img style={{transform: `rotate(${rotation}deg)`}} src={this.source} onClick={this.rotate} width="300" height="300" />
             </div>
     }
