b5c306f38c4d46d245a78175616666d62c26d6a9
Ena Hajdarevic <ena.h38@gmail.com>
2018-05-22 15:23:12 +0200

    Changed firstname validation restriction (from min 5 characters to 3)

--- SI Project/bulletin-board-front/src/components/login/Login.js
+++ SI Project/bulletin-board-front/src/components/login/Login.js
@@ -1,76 +1,112 @@
 import React, { Component } from 'react';
-import { Link, Redirect} from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import './Login.css';
 import Errors from '../../helpers/Errors';
 import { Validator, Handler, ErrorMessages } from '../../helpers/Validator';
 import axios from 'axios';
 
-
 class Login extends Component {
+	state = {
+		username: '',
+		password: '',
+		errors: new Errors(),
+		toDashboard: false,
+		toAdminboard: false
+	};
 
+	handleClick(e) {
+		axios
+			.post('/login', {
+				username: this.state.username,
+				password: this.state.password
+			})
+			.then(response => {
+				if (response.data)
+					response.data.role === 'admin'
+						? this.setState({ toAdminboard: true })
+						: this.setState({ toDashboard: true });
+				else {
+					alert('Bad credentials!');
+				}
+			})
+			.catch(error => console.log(error));
+	}
+	handleInput(e) {
+		if (!Handler[e.target.name](e.target.value)) {
+			this.state.errors.record({
+				[e.target.name]: ErrorMessages[e.target.name]
+			});
+			this.setState({
+				errors: this.state.errors,
+				[e.target.name]: e.target.value
+			});
+		} else {
+			this.state.errors.clear(e.target.name);
+			this.setState({
+				errors: this.state.errors,
+				[e.target.name]: e.target.value
+			});
+		}
+	}
 
-  state = {
-        username: "",
-        password: "",
-        errors: new Errors(),
-        toDashboard: false,
-        toAdminboard: false
-  };
+	areInputsNotEmpty() {
+		return Array.from(Object.keys(this.state)).every(
+			key =>
+				!(typeof this.state[key] === 'string' && this.state[key].length === 0)
+		);
+	}
 
-
-  handleClick(e) {
-      axios.post('/login', {
-        username: this.state.username,
-        password: this.state.password
-      })
-      .then(response => { 
-                             if(response.data) 
-                                response.data.role === "admin" ? this.setState({toAdminboard: true}) : this.setState({toDashboard: true});
-                              else{
-                                  alert('Bad credentials!')
-                              }
-                        })
-      .catch( error => console.log(error));
-  }
-  handleInput(e){
-      if (!Handler[e.target.name](e.target.value)){
-        this.state.errors.record({ [e.target.name]: ErrorMessages[e.target.name]});
-        this.setState({
-          errors: this.state.errors,
-          [e.target.name]: e.target.value
-        });
-      }else{
-        this.state.errors.clear(e.target.name);
-        this.setState({
-          errors: this.state.errors,
-          [e.target.name]: e.target.value
-        });
-      }
-  }
-
-  areInputsNotEmpty(){
-      return Array.from(Object.keys(this.state)).every(key => !(typeof this.state[key] === "string" && this.state[key].length === 0));
-  }
-                                                                                          
-  render() {
-    if (this.state.toDashboard === true) {
-      return <Redirect to='/dashboard' />
-    }
-    if (this.state.toAdminboard === true) {
-      return <Redirect to='/adminboard' />
-    }
-    return (
-        <div className="App-login">
-          <h3>Kako biste mogli koristiti stranicu, morate se prijaviti:</h3>
-          <input type="text" name="username" value= {this.state.username} placeholder="Username" onChange = { e => this.handleInput(e)}/><br/>
-          <span hidden={!this.state.errors.has('username')} className="help error">{this.state.errors.get('username')}</span><br/>
-          <input type="password" name="password" value= {this.state.password} placeholder="Password" onChange = { e => this.handleInput(e)}/><br/>
-          <span hidden={!this.state.errors.has('password')} className="help error">{this.state.errors.get('password')}</span><br/>
-          <button  disabled={this.state.errors.any() || !this.areInputsNotEmpty()} onClick={(e) => this.handleClick(e)}>Login</button><br/>
-            Nemate račun? <Link to="/register">Registrujte se!</Link>
-        </div>
-    );
-  }
+	render() {
+		if (this.state.toDashboard === true) {
+			return <Redirect to="/dashboard" />;
+		}
+		if (this.state.toAdminboard === true) {
+			return <Redirect to="/adminboard" />;
+		}
+		return (
+			<div className="App-login">
+				<h3>Kako biste mogli koristiti stranicu, morate se prijaviti:</h3>
+				<input
+					type="text"
+					name="username"
+					value={this.state.username}
+					placeholder="Username"
+					onChange={e => this.handleInput(e)}
+				/>
+				<br />
+				<span
+					hidden={!this.state.errors.has('username')}
+					className="help error"
+				>
+					{this.state.errors.get('username')}
+				</span>
+				<br />
+				<input
+					type="password"
+					name="password"
+					value={this.state.password}
+					placeholder="Password"
+					onChange={e => this.handleInput(e)}
+				/>
+				<br />
+				<span
+					hidden={!this.state.errors.has('password')}
+					className="help error"
+				>
+					{this.state.errors.get('password')}
+				</span>
+				<br />
+				<button
+					disabled={this.state.errors.any() || !this.areInputsNotEmpty()}
+					onClick={e => this.handleClick(e)}
+				>
+					Login
+				</button>
+				<br />
+				Nemate račun? <Link to="/register">Registrujte se!</Link>
+			</div>
+		);
+	}
 }
 
 export default Login;
--- SI Project/bulletin-board-front/src/helpers/Validator.js
+++ SI Project/bulletin-board-front/src/helpers/Validator.js
@@ -1,31 +1,36 @@
-
-let Validator=(function(){
-	
-	return{
-		validateUsername: username => username.length >= 5 && username.length < 15,
-		validatePassword: password => Boolean(password.match(/(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9].{7,})$/g)) ,
-		validateFirstname: firstname => firstname.length >= 5 && firstname.length < 15,
+let Validator = (function() {
+	return {
+		validateUsername: username => username.length >= 3 && username.length < 15,
+		validatePassword: password =>
+			Boolean(
+				password.match(/(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9].{7,})$/g)
+			),
+		validateFirstname: firstname =>
+			firstname.length >= 5 && firstname.length < 15,
 		validateLastname: lastname => lastname.length >= 5 && lastname.length < 15,
 		validateEmail: email => Boolean(email.match(/\S+@\S+\.\S+/)),
-		validateConfpassword: (confpassword,pass ) => confpassword === pass
-
-	}
-}());
+		validateConfpassword: (confpassword, pass) => confpassword === pass
+	};
+})();
 let Handler = {
 	username: Validator.validateUsername,
 	password: Validator.validatePassword,
 	firstname: Validator.validateFirstname,
-    lastname: Validator.validateLastname,
-    email: Validator.validateEmail,
-    confpassword: Validator.validateConfpassword
-}
+	lastname: Validator.validateLastname,
+	email: Validator.validateEmail,
+	confpassword: Validator.validateConfpassword
+};
 let ErrorMessages = {
-	username: "Invalid username! Username should contain beetween 5 and 15 characters.",
-	password: "Invalid password! Should contain min 7 characters and min one digit.",
-	firstname: "Invalid firstname! Firstname should contain beetween 5 and 15 characters.",
-    lastname: "Invalid lastname! Lastname should contain beetween 5 and 15 characters.",
-    email: "Invalid email! Email should contain beetween 5 and 25 characters.",
-    confpassword: "Password does not match the confirm password."
-}
+	username:
+		'Invalid username! Username should contain beetween 5 and 15 characters.',
+	password:
+		'Invalid password! Should contain min 7 characters and min one digit.',
+	firstname:
+		'Invalid firstname! Firstname should contain beetween 3 and 15 characters.',
+	lastname:
+		'Invalid lastname! Lastname should contain beetween 5 and 15 characters.',
+	email: 'Invalid email! Email should contain beetween 5 and 25 characters.',
+	confpassword: 'Password does not match the confirm password.'
+};
 
-export {Validator, Handler, ErrorMessages};
+export { Validator, Handler, ErrorMessages };
